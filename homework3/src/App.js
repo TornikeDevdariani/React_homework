593b3c8b60c41ee76ebf04e460672dcce75068a5
TornikeDevdariani <tornike.devdariani@gmail.com>
2023-10-20 18:19:47 +0400

    added key in app.js

--- homework3/src/App.js
+++ homework3/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <main className="root">
       {product.map((product) => (
         <Card
+          key={product.id}
           id={product.id}
           alt={product.alt}
           stock={product.stock}
